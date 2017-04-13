2d7c5ecb13dd39d605a7e23ef612e1a345f804f9
Deeksha <deeksha@Deekshas-MacBook-Pro.local>
2017-04-13 22:24:10 +1200

    Creating the mosaic using the tile from server

--- js/client.js
+++ js/client.js
@@ -39,13 +39,13 @@ function splitImage(image){
                 });
             }
             if(tiles.length > 0){
-                // drawTiles(tiles);
-                getColorForTile(tiles);
+                createMosaic(tiles);
             }
         }
     }
 }
 
+// For Testing if the split images can be displayed as mosaic
 function drawTiles(allTiles){
     for(var i = 0; i < allTiles.length; i++){
         var image       = document.createElement('img');
@@ -58,18 +58,43 @@ function drawTiles(allTiles){
     }
 }
 
-function getColorForTile(allTiles) {
+
+function createMosaic(allTiles) {
     for(var i = 0; i < allTiles.length; i++){
+        // Get the color of the tile image
         var image       = document.createElement('img');
         image.xdraw     = allTiles[i].x;
         image.ydraw     = allTiles[i].y;
         image.onload    = function () {
-            resultContext.drawImage(this, this.xdraw, this.ydraw);
-            var data = resultContext.getImageData(this.xdraw, this.ydraw, TILE_WIDTH, TILE_HEIGHT).data;
-            var hex = (256 + data[0]).toString(16).substr(1) +((1 << 24) + (data[1] << 16) | (data[2] << 8) | data[3]).toString(16).substr(1)
-            console.log(data[0]+','+data[1]+','+data[2]+','+data[3]+'='+hex);
+
+            var tileCanvas = document.createElement('canvas');
+            var context = tileCanvas.getContext('2d');
+            context.drawImage(this, this.xdraw, this.ydraw);
+            var data = context.getImageData(this.xdraw, this.ydraw, TILE_WIDTH, TILE_HEIGHT).data;
+            var hex = (256 + data[0]).toString(16).substr(1) +((1 << 24) + (data[1] << 16) | (data[2] << 8) | data[3]).toString(16).substr(1);
+
+            // Get the tile from Server
+            var url = 'http://localhost:8765'+'/color/'+hex;
+            var request = new XMLHttpRequest();
+            request.open('GET', url, true);
+
+            //Location to place the tile
+            var tileImageX = this.xdraw;
+            var tileImageY = this.ydraw;
+
+            // Place the svg received from server to the canvas
+            request.onload = function (response) {
+                if (request.readyState == XMLHttpRequest.DONE) {
+                    var mySrc = 'data:image/svg+xml;base64,'+window.btoa(request.responseText);
+                    var source = new Image();
+                    source.onload = function() {
+                        resultContext.drawImage(source, tileImageX, tileImageY);
+                    };
+                    source.src = mySrc;
+                }
+            };
+            request.send(null);
         };
-        image.src       = allTiles[i].imageUrl;
+        image.src = allTiles[i].imageUrl;
     }
 }
-
